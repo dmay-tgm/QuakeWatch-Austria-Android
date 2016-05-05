42ebe3429d52a083795c18432145af2643af20ba
Muehlehner Moritz <mmuehlehner@student.tgm.ac.at>
2016-05-05 12:14:09 +0200

    Added Erdbeben class

--- app/src/main/java/tgm/shakeit/quakewatchaustria/Erdbeben.java
+++ app/src/main/java/tgm/shakeit/quakewatchaustria/Erdbeben.java
@@ -1,5 +1,7 @@
 package tgm.shakeit.quakewatchaustria;
 
+import java.util.ArrayList;
+
 /**
  * Created by Moritz on 27.04.2016.
  */
@@ -45,18 +47,48 @@ public class Erdbeben {
         this.time = time;
     }
 
-    private double mag;
-    private String region;
-    private String ort;
-    private String date;
-    private String time;
+    public String getCords() {
+        return cords;
+    }
 
-    public Erdbeben (double mag, String reg, String ort, String date, String time){
+    public void setCords(String cords) {
+        this.cords = cords;
+    }
+
+    public String getDepth() {
+        return depth;
+    }
+
+    public void setDepth(String depth) {
+        this.depth = depth;
+    }
+
+    public ArrayList<String> getDist() {
+        return dist;
+    }
+
+    public void setDist(ArrayList<String> dist) {
+        this.dist = dist;
+    }
+
+    private double mag; //Magnitude
+    private String region; //Region: "Oberösterreich"
+    private String ort; //Ort bzw. Stadt: "Linz"
+    private String date; //Datum in Format: "1. Jan 2016"
+    private String time; //Zeit in Format: "04:20"
+    private String cords; //Koordinaten in Format: "47,34°N 14,54°O"
+    private String depth; //Tiefe mit Einheit: "7,6 km"
+    private ArrayList<String> dist; //Entfernungen zu anderen (relevanten) Orten: "3 km OSO von Wien"
+
+    public Erdbeben (double mag, String reg, String ort, String date, String time, String cords,String depth,ArrayList<String> dist){
         this.mag=mag;
         this.region=reg;
         this.ort=ort;
         this.date=date;
         this.time=time;
+        this.cords=cords;
+        this.depth=depth;
+        this.dist=dist;
     }
 
 
