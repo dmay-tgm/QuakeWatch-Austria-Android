b7e33c3d68d87fda7a7308e3df3de65f3175816f
Daniel May <dmay@student.tgm.ac.at>
2016-05-11 16:07:07 +0200

    Improve data interpretation
    The strategy is now to save the whole ArrayList of earquakes. Now the
    downloading, interpretation and saving are different tasks, therefore
    they belong to different classes.

--- app/src/main/java/tgm/shakeit/quakewatchaustria/Erdbeben.java
+++ app/src/main/java/tgm/shakeit/quakewatchaustria/Erdbeben.java
@@ -1,29 +1,132 @@
 package tgm.shakeit.quakewatchaustria;
 
+import android.location.Location;
 import android.util.Log;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.DateFormat;
+import java.io.Serializable;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
 
 /**
- * Interprets the received JSON data and provides a definition for earthquakes. Is used for displaying an earthquakes.
+ * Interprets the received JSON data and provides a definition for earthquakes. Is optimized for displaying an earthquakes.
  *
  * @author Daniel May
- * @version 2016-05-10.1
+ * @version 2016-05-11.1
  */
-public class Erdbeben {
+public class Erdbeben implements Serializable {
     private static final String TAG = "Erdbeben.java";
+    private final DecimalFormatSymbols decimalSymbol;
+    /**
+     * will be null if the given current location is also null
+     */
+    private String distance;// "7,0 km"
+    private double mag; //Magnitude
+    private String region; //Region: "Oberösterreich"
+    private String ort; //Ort bzw. Stadt: "Linz"
+    private String date; //Datum in Format: "1. Jan 2016"
+    private String time; //Zeit in Format: "04:20"
+    private String cords; //Koordinaten in Format: "47,34°N 14,54°O"
+    private String depth; //Tiefe mit Einheit: "7 km"
+    private ArrayList<String> dist; //Entfernungen zu anderen (relevanten) Orten: "3 km OSO von Wien"
+
+
+    /**
+     * Parses a feature JSON object to a valid earthquake object. If the user's GPS is not enabled, just pass null to the constructor and print out an error message.
+     *
+     * @param toParse feature JSON object
+     * @param current current geolocation of the user
+     */
+    public Erdbeben(JSONObject toParse, Location current) {
+        //german-like formatting
+        decimalSymbol = new DecimalFormatSymbols();
+        decimalSymbol.setDecimalSeparator(',');
+        decimalSymbol.setGroupingSeparator('.');
+        try {
+            JSONObject properties = toParse.getJSONObject("properties");
+            mag = properties.getDouble("mag");
+            String location = properties.getString("region");
+            //split the region into a city and a real region if it contains the delimiter
+            if (location.contains("/")) {
+                String[] splittedLocation = location.split("/");
+                region = splittedLocation[1].trim();
+                ort = splittedLocation[0].trim();
+            } else {
+                ort = "";
+                region = location.trim();
+            }
+            //Parse ISO8601 String to a DateTime object with the user's local timezone
+            DateTime date = ISODateTimeFormat.dateTimeParser().withOffsetParsed().parseDateTime(properties.getString("time")).withZone(DateTimeZone.getDefault());
+            //formatting the date
+            DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("d. MMM yyyy");
+            this.date = dateFormatter.print(date);
+            //formatting the time
+            DateTimeFormatter timeFormatter = DateTimeFormat.forPattern("HH:mm");
+            this.time = timeFormatter.print(date);
+            //get the coordinates
+            JSONArray coordinates = toParse.getJSONObject("geometry").getJSONArray("coordinates");
+            double latitude = coordinates.getDouble(0);
+            double longitude = coordinates.getDouble(1);
+            //formatting of a coordinate
+            DecimalFormat cordFormat = new DecimalFormat("0.0#", decimalSymbol);
+            StringBuilder sb = new StringBuilder();
+            sb.append(cordFormat.format(Math.abs(latitude))).append("°");
+            //append the correct direction
+            if (latitude > 0)
+                sb.append("N ");
+            else if (latitude < 0)
+                sb.append("S ");
+            else
+                sb.append("N/S ");
+            sb.append(cordFormat.format(Math.abs(longitude))).append("°");
+            if (longitude > 0)
+                sb.append("O");
+            else if (latitude < 0)
+                sb.append("W");
+            else
+                sb.append("O/W");
+            cords = sb.toString();
+            //rounding the depth
+            depth = Math.round(properties.getDouble("depth")) + " km";
+            //parse near places
+            JSONArray allPlaces = properties.getJSONArray("places");
+            dist = new ArrayList<>();
+            for (int i = 0; i < allPlaces.length(); i++)
+                dist.add(allPlaces.getJSONObject(i).getString("text"));
+            //creating Location object for the earthquake
+            Location quake = new Location("byLatLon");
+            quake.setLatitude(latitude);
+            quake.setLongitude(longitude);
+            //save the distance to the quake
+            refreshDistanceFromQuake(current, quake);
+        } catch (JSONException e) {
+            Log.e(TAG, "Couldn't interpret the data: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Calculates the distance from an earthquake's epicentre to the user's mobile phone.
+     *
+     * @param phone the mobile phone's current location
+     */
+    public void refreshDistanceFromQuake(Location phone, Location quake) {
+        distance = null;
+        if (phone != null) {
+            double tmp = phone.distanceTo(quake);
+            tmp /= 1000.0;
+            DecimalFormat distFormat = new DecimalFormat(",##0.0", decimalSymbol);
+            distance = distFormat.format(tmp) + " km";
+        }
+    }
 
     /**
      * Gets the magnitude in double format.
@@ -89,92 +192,20 @@ public class Erdbeben {
     }
 
     /**
+     * Gets the user's distance from an earthquake.
+     *
+     * @return the formatted distance.
+     */
+    public String getDistance() {
+        return distance;
+    }
+
+    /**
      * Gets the List of distances.
      *
      * @return list of distances to other places.
      */
     public ArrayList<String> getDist() {
         return dist;
-    }
-
-    private double mag; //Magnitude
-    private String region; //Region: "Oberösterreich"
-    private String ort; //Ort bzw. Stadt: "Linz"
-    private String date; //Datum in Format: "1. Jan 2016"
-    private String time; //Zeit in Format: "04:20"
-    private String cords; //Koordinaten in Format: "47,34°N 14,54°O"
-    private String depth; //Tiefe mit Einheit: "7,6 km"
-    private ArrayList<String> dist; //Entfernungen zu anderen (relevanten) Orten: "3 km OSO von Wien"
-
-    public Erdbeben(double mag, String reg, String ort, String date, String time/*, String cords,String depth,ArrayList<String> dist*/) {
-        this.mag = mag;
-        this.region = reg;
-        this.ort = ort;
-        this.date = date;
-        this.time = time;
-        //this.cords = cords;
-        //this.depth = depth;
-        //this.dist = dist;
-    }
-
-    /**
-     * Parses a feature JSON object to a valid earthquake object.
-     *
-     * @param toParse feature JSON object
-     */
-    public Erdbeben(JSONObject toParse) {
-        DecimalFormatSymbols decimalSymbol = new DecimalFormatSymbols();
-        decimalSymbol.setDecimalSeparator(',');
-        try {
-            JSONObject properties = toParse.getJSONObject("properties");
-            mag = properties.getDouble("mag");
-            String location = properties.getString("region");
-            if (location.contains("/")) {
-                String[] splittedLocation = location.split("/");
-                region = splittedLocation[1].trim();
-                ort = splittedLocation[0].trim();
-            } else {
-                ort = "";
-                region = location.trim();
-            }
-            DateFormat input = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ");
-            Date date = input.parse(properties.getString(time));
-            DateFormat outputTime = new SimpleDateFormat("HH:mm", Locale.GERMAN);
-            outputTime.setTimeZone(TimeZone.getDefault());
-            time = outputTime.format(date);
-            DateFormat outputDate = new SimpleDateFormat("d. MMM yyyy", Locale.GERMAN);
-            outputDate.setTimeZone(TimeZone.getDefault());
-            this.date = outputDate.format(date);
-            JSONArray coordinates = toParse.getJSONObject("geometry").getJSONArray("coordinates");
-            double latitude = coordinates.getDouble(0);
-            double longitude = coordinates.getDouble(1);
-            String formattedLatitude, formattedLongitude;
-            DecimalFormat cordFormat = new DecimalFormat("#0.0#", decimalSymbol);
-            formattedLatitude = cordFormat.format(Math.abs(latitude)) + "°";
-            formattedLongitude = cordFormat.format(Math.abs(longitude)) + "°";
-            if (latitude > 0)
-                formattedLatitude += "N ";
-            else if (latitude < 0)
-                formattedLatitude += "S ";
-            else
-                formattedLatitude += "N/S ";
-            if (longitude > 0)
-                formattedLongitude += "O";
-            else if (latitude < 0)
-                formattedLongitude += "W";
-            else
-                formattedLongitude += "O/W";
-            cords = formattedLatitude + formattedLongitude;
-            DecimalFormat depthFormat = new DecimalFormat("#0.0#", decimalSymbol);
-            depth = depthFormat.format(properties.getDouble("depth")) + " km";
-            JSONArray allPlaces = properties.getJSONArray("places");
-            dist = new ArrayList<>();
-            for (int i = 0; i < allPlaces.length(); i++)
-                dist.add(allPlaces.getJSONObject(i).getString("text"));
-        } catch (JSONException e) {
-            Log.e(TAG, "Couldn't interpret the data: " + e.getMessage());
-        } catch (ParseException e) {
-            Log.e(TAG, "Wrong datetime format: " + e.toString());
-        }
     }
 }
---
+++ app/src/main/java/tgm/shakeit/quakewatchaustria/FileManager.java
@@ -0,0 +1,68 @@
+package tgm.shakeit.quakewatchaustria;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Reads or writes generic data from/to a file using serialization.
+ *
+ * @author Daniel May
+ * @version 2016-05-11.1
+ */
+public class FileManager<T> {
+    public static final String EU_FILE = "eu.ser";
+    public static final String AT_FILE = "at.ser";
+    public static final String WORLD_FILE = "world.ser";
+    //public static final String LATEST_FILE = "latest.txt";
+
+    private static final String TAG = "FileManager.java";
+
+    /**
+     * Saves the given generic content in a file in the internal storage.
+     *
+     * @param filename the filename to save
+     * @param content  the generic content to save
+     * @param context  the ApplicationContext to use
+     */
+    public void writeObjects(String filename, T content, Context context) {
+        FileOutputStream outputStream;
+        try {
+            outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            ObjectOutputStream os = new ObjectOutputStream(outputStream);
+            os.writeObject(content);
+            os.close();
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error writing data: " + e.toString());
+        }
+    }
+
+    /**
+     * Reads the content from a file and parses it to a generic object.
+     *
+     * @param filename the file to readObjects from
+     * @param context  the ApplicationContext to use
+     */
+    public T readObjects(String filename, Context context) {
+        T desiredObject = null;
+        try {
+            FileInputStream inputStream = context.openFileInput(filename);
+            ObjectInputStream ois = new ObjectInputStream(inputStream);
+            desiredObject = (T) ois.readObject();
+            ois.close();
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "File was not found: " + e.toString());
+        } catch (IOException | ClassNotFoundException e) {
+            Log.e(TAG, "Error reading data: " + e.toString());
+        }
+        return desiredObject;
+    }
+}
